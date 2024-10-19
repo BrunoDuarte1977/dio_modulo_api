5758c608b7c7ff14c603a15d9e2f82bd182b632d
Bruno Duarte <brunoduarte1977@gmail.com>
2024-10-19 14:14:22 -0300

    Configurando a conexão

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using ModuloAPI.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<Contexto>(option =>
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DbConection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
